ad5250f9b40b5b3263e9f651c25152e471c87992
Pedro Bernardes <phec06@gmail.com>
2016-09-02 11:47:34 -0300

    Created log_entries route and api controller

---
+++ app/controllers/api/log_entries_controller.rb
@@ -0,0 +1,31 @@
+class Api::LogEntriesController < Api::BaseController
+  before_action :set_log_entry, only: [:destroy]
+
+  def index
+    @log_entries = current_user.log_entries
+  end
+
+  def create
+    create_params = log_entry_params
+    create_params[:user] = current_user
+
+    @log_entries = Log_Entry.create create_params
+    render :index
+  end
+
+  def destroy
+    @log_entry.destroy
+    render nothing: true
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_log_entry
+    @log_entry = current_user.log_entries.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def log_entry_params
+    params.require(:log_entry).permit(:name, :color, :parent_id)
+  end
+end
