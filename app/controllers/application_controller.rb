37beb68c89585fa511873db004c6ed2246d8e207
Pedro Bernardes <phec06@gmail.com>
2016-09-20 12:26:41 -0300

    Fixing timezone issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  around_action :set_timezone
+  # around_action :set_timezone
 
   def set_timezone
     if current_user
--- config/initializers/rabl.rb
+++ config/initializers/rabl.rb
@@ -1,4 +1,50 @@
 require 'rabl'
+# config/initializers/rabl.rb
+# monkey patch to format date automatically
+
+module Rabl
+  class Configuration
+    attr_accessor :force_iso_dates
+  end
+
+  class Builder
+    def to_hash(object = nil, settings = {}, options = {})
+      @_object = object           if object
+      @options.merge!(options)    if options
+      @settings.merge!(settings)  if settings
+
+      cache_results do
+        @_result = {}
+
+        # Merges directly into @_result
+        compile_settings(:attributes)
+
+        merge_engines_into_result
+
+        # Merges directly into @_result
+        compile_settings(:node)
+
+        replace_nil_values          if Rabl.configuration.replace_nil_values_with_empty_strings
+        replace_empty_string_values if Rabl.configuration.replace_empty_string_values_with_nil_values
+        remove_nil_values           if Rabl.configuration.exclude_nil_values
+        force_iso_dates             if Rabl.configuration.force_iso_dates
+
+        result = @_result
+        result = { @options[:root_name] => result } if @options[:root_name].present?
+        result
+      end
+    end
+
+    protected
+      def force_iso_dates
+        @_result = @_result.inject({}) do |new_hash, (k, v)|
+          new_hash[k] = v.respond_to?(:iso8601) ? v.iso8601 : v
+          new_hash
+        end
+      end
+  end
+end
+
 Rabl.configure do |config|
   # Commented as these are defaults
   # config.cache_all_output = false
@@ -24,4 +70,5 @@ Rabl.configure do |config|
   # config.replace_empty_string_values_with_nil_values = true # Defaults to false
   # config.exclude_nil_values = true # Defaults to false
   # config.exclude_empty_values_in_collections = true # Defaults to false
+  config.force_iso_dates = true
 end
