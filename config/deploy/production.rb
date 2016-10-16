5f74df3a3d5a74c5de6e0760d567a05f1a97e4e2
Pedro Bernardes <phec06@gmail.com>
2016-10-16 13:51:13 -0200

    Updated production DNS

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server 'ec2-52-67-178-12.sa-east-1.compute.amazonaws.com', user: 'ubuntu', roles: %w{app db web}
+server 'ec2-52-67-82-236.sa-east-1.compute.amazonaws.com', user: 'ubuntu', roles: %w{app db web}
 set :ssh_options, {
   keys: %w(~/.ssh/keys/personal-aws.pem),
   auth_methods: %w(publickey)
